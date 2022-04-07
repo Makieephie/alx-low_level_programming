bc096b59203d4ebb79a1e48a1c4bcc9e72b5b148
Makieephie <qchiamakaephraim@gmail.com>
2022-04-07 13:25:06 -0700

    correction of task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
-int main (void)
-
+/**
+ * print programming is like buildig a multilingual puzzle
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle.\n");
-	return(0);
+	return (0);
 }
