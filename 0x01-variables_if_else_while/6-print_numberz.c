349add2bc183afebad0b1ff706d20beef80e0c7d
Makieephie <qchiamakaephraim@gmail.com>
2022-04-11 14:30:55 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* adding header files */
+
+/**
+ *  main - this program print 0-9
+ *
+ *  Return: 0 if everything goes well
+ */
+int main(void)
+{
+	/* variable declarations */
+	int s;
+
+	/* loop for numbers 0-9 */
+	for (s = 0; s <= 9; s++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+
+	/* return 0 if it is successful */
+	return (0);
+}
