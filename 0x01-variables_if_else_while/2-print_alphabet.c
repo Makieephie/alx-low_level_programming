f1df827c32cc60c0e8a394c067411f03f7577b6f
Makieephie <qchiamakaephraim@gmail.com>
2022-04-11 08:12:40 -0700

    task 2 on alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* header file declaration */
+
+/* this program print alphabets in lower case */
+int main(void)
+{
+	/* variable declarations */
+	char s;
+
+	/* assigning values to the varaibles */
+	s = 'abcdefghijklmnopqrstuvwxyz';
+	/* printing ouput */
+	putchar(s);
+	putchar("\n");
+	/* return 0 if it is successful */
+	return (0);
+}
