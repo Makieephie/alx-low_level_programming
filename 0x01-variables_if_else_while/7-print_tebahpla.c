f08e2e01f64844b84dd61527560f3c22d0cf7324
Makieephie <qchiamakaephraim@gmail.com>
2022-04-11 15:30:38 -0700

    task 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* adding header files */
+
+/**
+ *  main - this program print lowercase 
+ *  letter in reverse order
+ *
+ *  Return: 0 if everything goes well
+ */
+int main(void)
+{
+	/* variable declarations */
+	int s;
+
+	/* loop for printing lowercase letter in reverse */
+	for (s = 25; s >= 0; s--)
+	{
+		putchar('a' + s);
+	}
+	putchar('\n');
+
+	/* return 0 if it is successful */
+	return (0);
+}
