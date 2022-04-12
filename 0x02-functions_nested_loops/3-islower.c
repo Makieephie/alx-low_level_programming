216da64006ff7c60028b2ecba1c09b186285d9b9
Makieephie <qchiamakaephraim@gmail.com>
2022-04-12 14:32:55 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* adding header files */
+
+/**
+ *  _islower - this program checks if a character is lowercase  or not
+ *
+ *  Return: 1 (success) or 0 (failure)
+ */
+int _islower(int c)
+{
+	/* check if c is lowercases */
+	if (c > 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
