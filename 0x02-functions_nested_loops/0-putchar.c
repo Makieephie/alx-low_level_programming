61f3927d43b2d7ca3e029e6a29e850020ffebf17
Makieephie <qchiamakaephraim@gmail.com>
2022-04-12 09:14:59 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* adding header files */
+
+/**
+ *  main - this program print _putchar
+ *
+ *  Return: 0 if everything goes well
+ */
+int main(void)
+{
+	/* variable declarations */
+	char s[] = '_putchar';
+	int i = 0;
+
+	/* loop through the letters */
+	while (i < 8)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	/* return 0 if it is successful */
+	return (0);
+}
