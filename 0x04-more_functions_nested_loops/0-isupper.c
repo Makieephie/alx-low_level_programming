f49c55dbed16528bd60510913781d50901f79fb3
Makieephie <qchiamakaephraim@gmail.com>
2022-04-19 08:43:39 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,10 +3,17 @@
 /**
  * _isupper - checks for uppercase
  * @c: character to check
- * Return: 1 or 0
+ * Return: 0
  */
 
 int _isupper(int c)
 {
-	return ((c >= 65) && (c <= 90));
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
