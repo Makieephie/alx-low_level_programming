cbddaff5b6863bcd440046f9ea1807375683b95f
Makieephie <qchiamakaephraim@gmail.com>
2022-04-19 14:57:01 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,7 +26,8 @@ int main(void)
 						putchar(i);
 						putchar(n);
 						putchar(j);
-						if (i == 55 && n == 56 && j == 57)
+						if (i == 55 && n == 56 &&
+								j == 57)
 						{
 							break;
 						}
