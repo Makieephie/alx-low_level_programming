6ed4c6591fff8cd751195d2feb6510dfc76b3ad0
makieephie <Qchiamakaephraim@gmail.com>
2022-05-24 08:54:43 -0700

    Task 5 correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * b - prints from init by the loader before main
+ * beforeMain - Runs before the main fucntion
  */
 
-void b(void) __attribute__ ((constructor));
-void b(void)
+void __attribute__((constructor)) beforeMain()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n%s",
+			"I bore my house upon my back!\n");
 }
